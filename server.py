53e616a5ac045b2ca33ce1b46add2fa4f390e73c
Tora Sirkka <tora@torasirkka.fi>
2021-07-25 19:56:23 -0700

    Route for file upload.

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def homepage():
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
